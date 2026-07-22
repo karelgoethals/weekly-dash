c3ae6a4f0395b92e38410bb12648a5d279ee4c82
karelgoethals <karel.goethals@omc.com>
2026-07-22 15:47:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ def delete_entry(entry_id):
 
 init_db()
 
-st.sidebar.title("Initiative + Orange Weekly Dashboard")
-page = st.sidebar.radio("Navigate", ["Add Data", "Dashboard", "Manage Weekly Data"])
+st.sidebar.title("Media Dashboard")
+page = st.sidebar.radio("Navigate", ["Add Entry", "Dashboard", "Manage Data"])
 
 if page == "Add Entry":
     st.title("Add Weekly Data")
